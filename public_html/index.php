7f6c0795f659971f6956c8667ea677e0ff7f37bb
Ville Valtonen <ville@vallaton.org>
2014-08-18 20:00:07 +0300

    initial commit

---
+++ public_html/index.php
@@ -0,0 +1,11 @@
+<?php
+    require "../resources/config.php";
+    require TEMPLATES_PATH . "/header.php";
+?>
+
+    <div class="row"></div>
+
+<?php
+    require TEMPLATES_PATH . "/footer.php";
+
+
---
+++ resources/templates/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <header>
+        <title>test</title>
+        <link type="text/css" src="css/bootstrap/3.2.0/bootstrap.css" />
+    </header>
+    <body>
+        <div class="container-fluid">
+
