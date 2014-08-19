6758464fc19e930238b2df2d7393e86f00fdae3f
vivalton <ville.valtonen@oamk.fi>
2014-08-19 08:54:56 +0300

    demo

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
-<!--
+<!-- this block breaks css autocompletion -->
 <style type="text/css">
     .test {
         font-size: 120%;
     }
 </style>
-<!-- -->
+
+<!-- expected behavior: suggest .first, .second and .test -->
+<!-- now it only suggests .test -->
 <table class=""></table>
 
